c773285150912bc5ddc6919da19f7eeced43ad79
muskansinghrajput <56830498+muskansinghrajput@users.noreply.github.com>
2019-10-21 23:34:34 +0530

    Add files via upload

---
+++ bubble1.py
@@ -0,0 +1,24 @@
+
+import random as rd
+
+def bubbleSort(nums):
+    for passnum in range(len(nums)-1,0,-1):
+        for i in range(passnum):
+            if nums[i]>nums[i+1]:
+                temp = nums[i]
+                nums[i] = nums[i+1]
+                nums[i+1] = temp
+nums=[]
+for i in range(100):
+    nums.append(rd.randint(10,90))
+
+bubbleSort(nums)
+print(nums)
+
+n=int(input("Enter the element to search:"))
+if n in nums:
+    print(nums.index(n))
+else:
+    print("Not Found")
+print("Inorder Traversal of the Binary Search Tree is :",nums)
+
